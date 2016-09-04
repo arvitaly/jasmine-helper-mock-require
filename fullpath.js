e9d0cdbee749ae144b66fd7a44b8040dcd344bff
Alexandro <arvitaly@list.ru>
2016-09-05 05:12:32 +0700

    change path resolve

--- fullpath.js
+++ fullpath.js
@@ -1,25 +1,24 @@
-//Get from https://github.com/boblauer/mock-require/blob/master/index.js
-var Module = require('module'),
-    join = require('path').join,
-    dirname = require('path').dirname;
-module.exports = function (path, calledFrom) {
-    var resolvedPath;
-    try {
-        resolvedPath = require.resolve(path);
-    } catch (e) { }
-
-
-    var isExternal = /[/\\]node_modules[/\\]/.test(resolvedPath);
-    var isSystemModule = resolvedPath === path;
-    if (isExternal || isSystemModule) {
-        return resolvedPath;
+var path = require('path'), fs = require('fs');
+module.exports = (modulePath, basePath, npmPath) => {
+    npmPath = npmPath || process.cwd() + "/node_modules";
+    var fullpath;
+    if (path.isAbsolute(modulePath)) {
+        //Local module with absolute path
+        fullpath = modulePath;
+    } else {
+        //Local module with relative path
+        if (modulePath.substr(0, 2) === "./") {            
+            fullpath = path.resolve(path.join(basePath, modulePath));
+        } else {
+            //npm extern module            
+            try {
+                fullpath = path.resolve(path.join(npmPath, modulePath))
+                fs.accessSync(fullpath, fs.F_OK);
+            } catch (e) {
+                //System module or non-existing
+                fullpath = modulePath;
+            }
+        }
     }
-
-    var isLocalModule = /^\.{1,2}[/\\]/.test(path);
-    if (!isLocalModule) {
-        return path;
-    }
-
-    var localModuleName = join(dirname(calledFrom), path);    
-    return Module._resolveFilename(localModuleName);
+    return require.resolve(fullpath);
 }
