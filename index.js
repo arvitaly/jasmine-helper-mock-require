454b37d65e29e65c51c57e65c66e866ed70598f3
Alexandro <arvitaly@list.ru>
2016-09-05 23:00:16 +0700

    change fullpath by resolve-module-path

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 var Module = require('module'),
-    fullpath = require('./fullpath'),
-    callerId = require('caller-id'),
+    resolve = require('resolve-module-path'),
     Path = require('path');
 var mock = {
     require: function (path, mocks) {
         var originalLoader = Module._load;
         var realMocks = {};
-        var calledFrom = Path.dirname(callerId.getData().filePath);
-        var realPath = fullpath(path, calledFrom);
+        //var calledFrom = Path.dirname(callerId.getData().filePath);
+        var realPath = resolve(path, {
+            stackDepth: 1
+        });
         var realPathDir = Path.dirname(realPath);
         if (mocks) {
             for (var p in mocks) {
-                realMocks[fullpath(p, calledFrom)] = mocks[p];
+                realMocks[resolve(p, {
+                    stackDepth: 1
+                })] = mocks[p];
             }
         }
         Module._load = function () {
             var newModule = Object.assign({}, Module.prototype);
-            newModule.realRequire = newModule.require; 
+            newModule.realRequire = newModule.require;
             newModule.require = function (requirePath) {
-                var realRequest = fullpath(requirePath, realPathDir);
+                var realRequest = resolve(requirePath, {
+                    basePath: realPathDir
+                });
                 if (realMocks[realRequest]) {
                     return realMocks[realRequest];
                 } else {
