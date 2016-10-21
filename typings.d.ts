7bc3737d0d7bf4b67228281e102f7cb4907ef4db
Alexandro <arvitaly@list.ru>
2016-10-21 14:50:43 +0700

    fix typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,5 @@
 export function installMockPromise(): void;
 export function uninstallMockPromise(): void;
-export function require(modulePath: string, mocks: { [index: string]: any }): void;
+export function require(modulePath: string, mocks: { [index: string]: any }): any;
 export function installSyncFS(fs): void;
 export function uninstallFSSync(fs): void;
