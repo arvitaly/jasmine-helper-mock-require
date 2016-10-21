491e66d29b2e0540fd4d10837839df29525d8105
Alexandro <arvitaly@list.ru>
2016-10-21 13:45:05 +0700

    fix typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,10 +1,3 @@
-declare module "mock2" {
-    function installMockPromise(): void;
-    function uninstallMockPromise(): void;
-    function require(modulePath: string, mocks: { [index: string]: any }): void;
-    export = {
-        installMockPromise: installMockPromise,
-        uninstallMockPromise: uninstallMockPromise,
-        require: require
-    }
-}
+export function installMockPromise(): void;
+export function uninstallMockPromise(): void;
+export function require(modulePath: string, mocks: { [index: string]: any }): void;
